1266f9ac4564667d3ba16c4e512df7aec0cc1522
Vladislav Chirkov <mac@MacBook-Air.local>
2018-10-25 21:45:46 +0200

    test kartinki

--- EMA/ViewController.swift
+++ EMA/ViewController.swift
@@ -11,16 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tiPidorLabel: UILabel!
     
+    @IBOutlet weak var whatAHandsomeBoiii: UIImageView!
     @IBOutlet weak var knopka: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        whatAHandsomeBoiii.isHidden = true
         knopka.layer.borderWidth = 1
         knopka.layer.borderColor = UIColor.blue.cgColor
         knopka.layer.cornerRadius = 5
+        whatAHandsomeBoiii.layer.cornerRadius = 5
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func onKnopkaTap(_ sender: Any) {
         tiPidorLabel.text = "ТЫ ПИДОР"
+        knopka.isHidden = true
+        whatAHandsomeBoiii.isHidden = false
+        
     }
     
 
