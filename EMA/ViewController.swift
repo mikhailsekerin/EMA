25807ce8135e99c68ed03aaa704e3fca8d59c0c3
Михаил Секерин <mihailsekerin@Mihails-MacBook-Pro.local>
2018-10-25 11:31:31 +0200

    first commit

---
+++ EMA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMA
+//
+//  Created by Михаил Секерин on 25.10.18.
+//  Copyright © 2018 Mikhail Sekerin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
