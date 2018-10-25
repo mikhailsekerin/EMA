4b811bb26e03830bda594b011d42daf8562d1a34
Vladislav Chirkov <mac@MacBook-Air.local>
2018-10-25 15:59:46 +0200

    test

--- EMA/ViewController.swift
+++ EMA/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tiPidorLabel: UILabel!
+    
+    @IBOutlet weak var knopka: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        knopka.layer.borderWidth = 1
+        knopka.layer.borderColor = UIColor.blue.cgColor
+        knopka.layer.cornerRadius = 5
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func onKnopkaTap(_ sender: Any) {
+        tiPidorLabel.text = "ТЫ ПИДОР"
+    }
+    
 
 }
 
